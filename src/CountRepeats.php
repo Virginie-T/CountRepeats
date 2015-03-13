e61b55076c215a893d0b0e100aed0211192d5cfb
virginie <virginietrubiano@gmail.com>
2015-03-13 10:56:23 -0700

    all the tests added and methods (working)

--- src/CountRepeats.php
+++ src/CountRepeats.php
@@ -2,12 +2,20 @@
 
 	class CountRepeats {
 
-		function countRepeats($input_letter, $find_input) {
+		function count_repeats($input_string, $find_input) {
 
+			$lc_string = strtolower($input_string);
+			$lc_find = strtolower($find_input);
+			$exploded_string = explode(" ", $lc_string);
+			$number = array();
+				foreach ($exploded_string as $word) {
 
-
-
-
+					if ($lc_find == $word) {
+						array_push($number, 1);
+					} 
+				}
+			$output = array_sum($number);
+			return $output;
 
 
 
--- tests/CountRepeatsTest.php
+++ tests/CountRepeatsTest.php
@@ -1,10 +1,10 @@
 <?php 
 
-	require_once "src/CountRepeats.php";
+	require_once 'src/CountRepeats.php';
 
 	class CountRepeatsTest extends PHPUnit_Framework_TestCase
 	{
-		function single_letter() 
+		function test_singleLetter() 
 		{
 			//arrange
 			$test_count = new CountRepeats;
@@ -12,17 +12,110 @@
 			$find_input = "a";
 
 			//act
-			$result = $test_count->countRepeats($input_letter, $find_input);
+			$result = $test_count->count_repeats($input_letter, $find_input);
 
 			//assert
 			$this->assertEquals(1, $result);
 		}
 
+		function test_multiLetter() 
+		{
+			//arrange
+			$test_count = new CountRepeats;
+			$input_letters = "a b";
+			$find_input = "a";
 
+			//act
+			$result = $test_count->count_repeats($input_letters, $find_input);
 
+			//assert
+			$this->assertEquals(1, $result);
+		}
 
+		function test_duplicateLetter() 
+		{
+			//arrange
+			$test_count = new CountRepeats;
+			$input_letters = "a a";
+			$find_input = "a";
 
+			//act
+			$result = $test_count->count_repeats($input_letters, $find_input);
 
+			//assert
+			$this->assertEquals(2, $result);
+		}
+
+		function test_multiDuplicateLetter() 
+		{
+			//arrange
+			$test_count = new CountRepeats;
+			$input_letters = "a b a";
+			$find_input = "a";
+
+			//act
+			$result = $test_count->count_repeats($input_letters, $find_input);
+
+			//assert
+			$this->assertEquals(2, $result);
+		}
+
+		function test_singleWord() 
+		{
+			//arrange
+			$test_count = new CountRepeats;
+			$input_word = "cat";
+			$find_input = "cat";
+
+			//act
+			$result = $test_count->count_repeats($input_word, $find_input);
+
+			//assert
+			$this->assertEquals(1, $result);
+		}
+
+		function test_singleWordNotMatching() 
+		{
+			//arrange
+			$test_count = new CountRepeats;
+			$input_word = "cat";
+			$find_input = "dog";
+
+			//act
+			$result = $test_count->count_repeats($input_word, $find_input);
+
+			//assert
+			$this->assertEquals(0, $result);
+		}
+
+		function test_string() 
+		{
+			//arrange
+			$test_count = new CountRepeats;
+			$input_word = "the last time I saw him, he was at the bar";
+			$find_input = "the";
+
+			//act
+			$result = $test_count->count_repeats($input_word, $find_input);
+
+			//assert
+			$this->assertEquals(2, $result);
+		}
+		
+		function test_lcString() 
+		{
+			//arrange
+			$test_count = new CountRepeats;
+			$input_word = "The last time I saw him, he was at the bar";
+			$find_input = "tHe";
+
+			//act
+			$result = $test_count->count_repeats($input_word, $find_input);
+
+			//assert
+			$this->assertEquals(2, $result);
+		}
+		
 	}
 
 ?>
